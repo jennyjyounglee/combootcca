17aab65e367d6ba8fe3b1cbbe3cd690ba93bdaeb
Dan Kessler <kesslerd@umich.edu>
2021-04-12 10:00:07 -0400

    write more code

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -1,12 +1,13 @@
-##' .. content for \description{} (no empty lines) ..
+##' Bootstrap data and 
 ##'
 ##' .. content for \details{} ..
-##' @title bootstrap CCA
+##' @title bootstrap CCA with Sign Fixing
 ##' @param X Data matrix of size N by p
 ##' @param Y Data matrix of size N by q
 ##' @param nboots Number of bootstrap sample to draw
 ##' @return 
 ##' @author Daniel Kessler
+##' @export
 bootstrapcca <- function(X, Y, nboots){
     N <- nrow(X)
     fm <- cancor(X, Y)
@@ -20,10 +21,58 @@ bootstrapcca <- function(X, Y, nboots){
 
     for (i in 1:nboots){
         idx <- sample(N, replace=TRUE)
-        fm.boot <- cancor(X[idx,], Y[idx,])
+        fm.boot <- cancor.signfix(X[idx,], Y[idx,])
         rho.boot[,i] <- fm.boot$cor
         beta.boot[,,i] <- fm.boot$ycoef
         alpha.boot[,,i] <- fm.boot$xcoef
     }
     
+}
+
+##' Compute canonical correlations between two data matrices, but
+##' impose identifiability conditions so the signs are fixed rather
+##' than random
+##'
+##' Canonical Correlation Analysis suffers from sign ambiguity in the
+##' estimated coefficients. This is because Corr(U, V) = Corr(-U, -V)
+##' and relatedly obtains from the SVD decomposition typically used to
+##' solve the CCA problem. This function adopts the convention that
+##' each canonical coefficient pair (xcoef, ycoef) should satisfy the
+##' condition that its maximal element (in absolute value) has
+##' positive sign. You may wish to standardize the columns of X and Y
+##' prior to calling this function so that differences in variable
+##' scaling do not dominate the identity of the maximal element.
+##' @title Sign-Fixed Canonical Correlations
+##' @param ... Arguments passed on to cancor
+##' @return Same object as returned by cancor after sign-flipping per
+##'     the identifiability condition discussed in Details.
+##' @author Daniel Kessler
+cancor.signfix <- function(...){
+    fm <- cancor(...)
+
+    K <- min(ncol(fm$xcoef), ncol(fm$ycoef))
+
+    theta <- rbind(fm$xcoef[,1:K], fm$ycoef[,1:K])
+    
+    maxes <- apply(theta, 2, absmax)
+
+    flip <- diag(sign(maxes))
+
+    fm$xcoef[,1:K] <- fm$xcoef[,1:K] %*% flip
+    fm$ycoef[,1:K] <- fm$ycoef[,1:K] %*% flip
+
+    return(fm)
+    
+}
+
+##' Return the (signed) value of the maximum (in magnitude) element of a vector
+##'
+##' An example is especially illustrative. absmax(c(1,-3,2)) will
+##' yield -3. absmax(c(1,-2,3)) will yield 3.
+##' @title Find Maximum Magnitude Element of Vector
+##' @param x
+##' @return the signed maximum
+##' @author Daniel Kessler
+absmax <- function(x){
+    x[which.max( abs(x) )]
 }
