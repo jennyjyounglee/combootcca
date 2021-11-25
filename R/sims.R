7e2ef0029930ff1c8e073953b40feacc6b985811
Dan Kessler <kesslerd@umich.edu>
2021-11-24 21:09:53 -0500

    define some functions for simulations in the package

--- R/sims.R
+++ R/sims.R
@@ -25,3 +25,58 @@ sim.coverage.check <- function(n, px, py){
     boot.cis <- bootstrapcca(X, Y)
 
 }
+
+coverage_asymptotic <- function(n, p, q, reps) {
+  sigma <- gen_sigma(p, q)
+
+  sigma_r <- chol(sigma)
+  fm_true <- cancor.cov(sigma, px = p)
+
+  cover <- list()
+  cover$x <- array(NA, c(dim(fm_true$xcoef), reps))
+  cover$y <- array(NA, c(dim(fm_true$ycoef), reps))
+
+  for (i in 1:reps) {
+    sigma <- gen_sigma(p, q)
+    sigma_r <- chol(sigma)
+    fm_true <- cancor.cov(sigma, px = p)
+
+    newdata <- t(t(sigma_r) %*% matrix(rnorm(n * (p + q)), p + q, n))
+    x <- newdata[, 1:p]
+    y <- newdata[, (p + 1):(p + q)]
+
+    ci_asymptotic <- cca_ci_asymptotic(x, y)
+
+    cover$x[, , i] <- ci_asymptotic$xcoef[, , 1] < fm_true$xcoef &
+      fm_true$xcoef < ci_asymptotic$xcoef[, , 2]
+
+    cover$y[, , i] <- ci_asymptotic$ycoef[, , 1] < fm_true$ycoef &
+      fm_true$ycoef < ci_asymptotic$ycoef[, , 2]
+  }
+  return(cover)
+}
+
+gen_sigma <- function(p, q) {
+  sxx <- diag(p)
+  syy <- diag(q)
+
+  sxx_sqrt <- expm::sqrtm(sxx)
+  syy_sqrt <- expm::sqrtm(syy)
+
+  qx <- pracma::randortho(p)
+  qy <- pracma::randortho(q)
+
+  rho <- sort(runif(min(p, q)), decreasing = TRUE)
+  s <- matrix(0, q, p)
+  diag(s) <- rho
+
+  syx <- syy_sqrt %*% qy %*% s %*% t(qx) %*% sxx_sqrt
+
+  sigma <- matrix(0, p + q, p + q)
+  sigma[1:p, 1:p] <- sxx
+  sigma[(p + 1):(p + q), (p + 1):(p + q)] <- syy
+  sigma[(p + 1):(p + q), 1:p] <- syx
+  sigma[1:p, (p + 1):(p + q)] <- t(syx)
+
+  return(sigma)
+}
