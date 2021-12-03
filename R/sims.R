620f8883bf162032bb9b42a26cf270f84c364e82
Dan Kessler <kesslerd@umich.edu>
2021-12-03 09:37:06 -0500

    add bug-fixed version of randortho

--- R/sims.R
+++ R/sims.R
@@ -80,3 +80,27 @@ gen_sigma <- function(p, q) {
 
   return(sigma)
 }
+
+## like pracma::randortho but without the bug
+randortho_fixed <- function(n, type = c("orthonormal", "unitary")) {
+    stopifnot(is.numeric(n), length(n) == 1,
+              floor(n) == ceiling(n), n >= 1)
+    if (n == 1)
+        return(matrix(1, 1, 1))
+
+    type <- match.arg(type)
+    if (type == "orthonormal") {
+        z <- pracma::randn(n, n) / sqrt(2.0)
+    } else {
+        z <- (pracma::randn(n, n) + 1i * pracma::randn(n, n)) / sqrt(2.0)
+    }
+
+    # QR decomposition for real or complex matrices
+    Z <- qr(z)
+    q <- qr.Q(Z)
+    r <- qr.R(Z)
+
+    d <- diag(r)
+    ph <- d / abs(d)
+    q %*% diag(ph)
+}
