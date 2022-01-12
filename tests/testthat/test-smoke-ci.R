39d3d9f5afed0f2ffa34dc0989004237624e8e42
Dan Kessler <kesslerd@umich.edu>
2022-01-12 16:44:26 -0500

    smoke test new inner function

--- tests/testthat/test-smoke-ci.R
+++ tests/testthat/test-smoke-ci.R
@@ -11,4 +11,12 @@ test_that("All methods for CCA confidence intervals run without errors", {
   expect_error(cca_ci_bootstrap_abs(x, y, parametric = FALSE), NA)
   expect_error(cca_ci_bootstrap_abs(x, y, parametric = TRUE), NA)
 })
+
+test_that("Inner BOOT Function Works", {
+
+ problem <- bt_problem_std_fun(p = 2, q = 3, n = 1000, inreps = 3L)
+
+ out <- bt_algo_boot_inner(problem$inreps[[1]], 1000, align = cca_align_posdiag,
+                           level = .95)
+
 })
