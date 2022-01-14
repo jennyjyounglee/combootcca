04e5665a5802127e94e98a37e11cb7066cec3c38
Dan Kessler <kesslerd@umich.edu>
2022-01-13 20:31:10 -0500

    and update tests accordingly

--- tests/testthat/test-smoke-ci.R
+++ tests/testthat/test-smoke-ci.R
@@ -10,15 +10,7 @@ test_that("All methods for CCA confidence intervals run without errors", {
   expect_error(cca_ci_regression(x, y), NA)
   expect_error(cca_ci_absboot(x, y, parametric = FALSE), NA)
   expect_error(cca_ci_absboot(x, y, parametric = TRUE), NA)
+  expect_error(cca_ci_boot(x, y), NA)
 })
 
-test_that("Inner BOOT Function Works", {
 
- problem <- bt_problem_std_fun(p = 2, q = 3, n = 1000, inreps = 3L)
-
- expect_error(bt_algo_boot_inner(problem$inreps[[1]], 1000,
-   align = cca_align_posdiag,
-   level = .90
- ), NA)
-
-})
