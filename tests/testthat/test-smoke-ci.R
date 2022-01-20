2e8cbc8f7585c54c80de83051f91645f79fc862a
Dan Kessler <kesslerd@umich.edu>
2022-01-19 19:25:38 -0500

    WIP: add some more smoke test

--- tests/testthat/test-smoke-ci.R
+++ tests/testthat/test-smoke-ci.R
@@ -12,3 +12,27 @@ test_that("All methods for CCA confidence intervals run without errors", {
   expect_error(cca_ci_absboot(x, y, parametric = TRUE), NA)
   suppressWarnings(expect_error(cca_ci_boot(x, y), NA))
 })
+
+test_that("Coverage Assessments Works", {
+
+  n <- 1e3
+  p <- 20
+  q <- 20
+
+  Sigma <- gen_sigma(p, q)
+  fm_true <- cancor_cov(Sigma, px = p)
+
+  dat <- gen_data(Sigma, p, q, n)
+
+  fm_hat <- cancor_scaled(dat$x, dat$y)
+
+  cis <- cca_ci_regression(dat$x, dat$y, align = cca_align_greedy_cosx, ref = fm_hat)
+
+  cca_ci_coverage_possibilities(fm_true, cis)
+
+  cca_ci_coverage_pangloss(fm_true, cis)
+
+  cca_ci_coverage_signflip(fm_true, cis)
+
+
+})
